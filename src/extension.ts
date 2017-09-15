c1c72186acd2113fc30ff78ad8bbfe0c2db80eca
Leif Persson <leifmariposa@hotmail.com>
2017-09-15 20:48:37 +0200

    First test of listing files in a folder

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,96 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+//import * as commands from './commands';
+
+export interface FileData {
+    label: string;
+    name: string;
+    path: string;
+    isFile: boolean;
+    isDirectory: boolean;
+}
+
+let cwd: string = '';
+
+export function checkError(err: NodeJS.ErrnoException): boolean {
+    if (err) {
+        vscode.window.showErrorMessage(err.message);
+    }
+
+    return !!err;
+}
+
+export function showFileList(dir?: string): void {
+    dir = dir || cwd;
+
+    if (!dir) { return; }
+
+    try {
+        const stats: fs.Stats = fs.lstatSync(dir);
+
+        if (!stats.isDirectory()) { return; }
+    } catch (err) {
+        checkError(err);
+
+        return;
+    }
+
+    cwd = dir;
+
+    fs.readdir(cwd, (err, results) => {
+        if (checkError(err)) { return; }
+
+        // Build a lookup table
+        const files: FileData[] = results.reduce((arr, file) => {
+            try {
+                const fullPath: string = path.join(cwd, file);
+                const stats: fs.Stats = fs.lstatSync(fullPath);
+                const isFile = stats.isFile();
+                const isDirectory = stats.isDirectory();
+
+                if (isFile || isDirectory) {
+                    arr.push({
+                        label: file,
+                        name: file,
+                        path: fullPath,
+                        isFile,
+                        isDirectory,
+                    });
+                }
+            } catch (err) { }
+
+            return arr;
+        }, []);
+
+        //const cmdData = { cwd, files };
+        //const options: string[] = commands.getList('top', cmdData).concat(
+        //    files.map(file => file.label),
+        //    commands.getList('bottom', cmdData)
+        //);
+
+        const options: string[] = files.map(file => file.label);
+
+        vscode.window.showQuickPick(options).then(label => {
+            if (!label) { return; }
+
+            const file: FileData = files.find(file => file.label === label);
+
+            // If a command is being run then don't show the default list of files and folders
+            //if (commands.handle(label, cmdData)) { return; }
+
+            if (file.isDirectory) {
+                showFileList(file.path);
+            } else if (file.isFile) {
+                vscode.workspace.openTextDocument(file.path).then(doc => {
+                    vscode.window.showTextDocument(doc);
+                });
+            }
+        });
+    });
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -21,8 +111,10 @@ export function activate(context: vscode.ExtensionContext) {
         const folders: string[] = config.get('folders', '').split('|');
         const ff = config.get('folders', '');
 
+        showFileList("c:\\dropbox\\info");
+
         // Display a message box to the user
-        vscode.window.showInformationMessage(ff);
+        //vscode.window.showInformationMessage(ff);
     });
 
     context.subscriptions.push(disposable);
